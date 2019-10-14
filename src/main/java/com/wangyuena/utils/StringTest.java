6c0f69b977d87852de2b87e60a366bb538b3ea4d
wangyuenalonglive <3535200598@qq.com>
2019-10-14 09:51:06 +0800

    测试周考2

---
+++ src/main/java/com/wangyuena/utils/StringTest.java
@@ -0,0 +1,41 @@
+package com.wangyuena.utils;
+
+import java.util.Scanner;
+
+/**
+ * @author WYN
+ * @time 2019年10月14日
+ */
+public class StringTest {
+	public static void main(String[] args) {
+		StringTest ss = new StringTest();
+		//ss.hasText();
+		ss.isNumber();
+	}
+/**
+ * 判断是否是控制
+ */
+	protected void hasText() {
+		System.out.println("请输入字符串：");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		if(str!="") {
+			System.out.println("字符串有值！");
+		}else if(str=="" && str==" "){
+			System.out.println("字符串为空值");
+		}
+	}
+	/***
+	 * 判断是否是数字
+	 */
+	protected void isNumber() {
+		System.out.println("请输入数字：");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		if(num>=0) {
+			System.out.println(num+"是数字");
+		}else {
+			System.out.println("无法识别");
+		}
+	}
+}
